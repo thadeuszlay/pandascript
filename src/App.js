204238902f55d91ccee949085fdfa0f9abb30827
thanle <thanh.le@sinnerschrader.com>
2018-10-21 11:28:19 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    console.log("panda is alive");
     return (
       <div className="App">
         <header className="App-header">
