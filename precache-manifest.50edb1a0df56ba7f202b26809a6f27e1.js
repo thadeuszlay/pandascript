1340648253144ff2cdf58caab6256da390da2e47
thanle <thanh.le@sinnerschrader.com>
2018-10-21 11:23:26 +0200

    Updates

---
+++ precache-manifest.50edb1a0df56ba7f202b26809a6f27e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pandascript/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bcbca1984aa0785e3649",
+    "url": "/pandascript/static/js/runtime~main.bcbca198.js"
+  },
+  {
+    "revision": "87f04a05ec148eaa4c74",
+    "url": "/pandascript/static/js/main.87f04a05.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/pandascript/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "87f04a05ec148eaa4c74",
+    "url": "/pandascript/static/css/main.bba1ff8e.chunk.css"
+  },
+  {
+    "revision": "9f32f4e4d40d361cfcabf3a2ac965b2a",
+    "url": "/pandascript/index.html"
+  }
+];
