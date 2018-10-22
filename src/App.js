b7192dfdff8cd2af3749eb5e4f146a0c4cb7492e
thanle <thanh.le@sinnerschrader.com>
2018-10-22 22:22:51 +0200

    pic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './panda.png';
 import './App.css';
 
 class App extends Component {
   render() {
-    console.log("panda is alive test test");
+    console.log('panda is alive test test');
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to PandaScript</h1>
           <h1>Impressum</h1>
           <p>Thanh Le</p>
