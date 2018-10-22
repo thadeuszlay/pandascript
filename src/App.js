3edb85de77a2400a618be7c9cc2f90ab6160c1d6
thanle <thanh.le@sinnerschrader.com>
2018-10-22 21:59:07 +0200

    add

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Welcome to PandaScript</h1>
+          <h1>Impressum</h1>
+          <p>Thanh Le</p>
+          <p>Schwetzingenstr. 10</p>
+          <p>81243 Muenchen</p>
           <a
             className="App-link"
             href="https://reactjs.org"
